c2fd8d7f0610f22ce80765c5288d1b8d127fdaa6
Alexander Schau <alexander@schau.email>
2022-04-09 19:57:28 +0000

    add custom thumbnails

---
+++ src/pages/img/[data].ts
@@ -0,0 +1,33 @@
+import { decode } from "js-base64";
+import moment from "moment-timezone";
+
+export async function get({ data }) {
+  let meetup: MeetupType;
+  try {
+    meetup = JSON.parse(decode(data));
+  } catch {
+    return new Response(null, {
+      status: 404,
+      statusText: "Not found",
+    });
+  }
+
+  const resp = await fetch(
+    `https://img.alexander.sbs/v1/image?tkn=${
+      import.meta.env.IMAGE_API_KEY
+    }&url=https://meetup.day/og&h=1000&w=1500&title=${encodeURIComponent(
+      meetup.title || "404 - Page not found"
+    )}&date=${encodeURIComponent(
+      new Date(meetup.from * 1000).toDateString() +
+        ", " +
+        moment(meetup.from * 1000)
+          .tz("UTC")
+          .format("HH.mm") +
+        " UTC" || ""
+    )}`
+  );
+
+  return new Response(resp.body, {
+    headers: resp.headers,
+  });
+}
