2d563af6fa751830a861757ccf2d83661bfd5572
Alexander Schau <alexander@schau.email>
2022-05-03 16:00:17 +0000

    direct image link

---
+++ src/components/genImageUrls.ts
@@ -0,0 +1,28 @@
+import moment from "moment-timezone";
+import jwt from "jsonwebtoken";
+
+export const getImageUrl=(meetup:MeetupType):string=>{
+
+  const token = jwt.sign(
+    {
+      token_id: "1",
+      url: "https://meetup.day/og",
+      h: 1000,
+      w: 1500,
+      params: {
+        title: meetup.title || "404 - Page not found",
+        date:
+          new Date(meetup.from * 1000).toDateString() +
+            ", " +
+            moment(meetup.from * 1000)
+              .tz("UTC")
+              .format("HH.mm") +
+            " UTC" || "",
+      },
+    },
+    process.env.IMAGE_API_KEY || import.meta.env.IMAGE_API_KEY
+  );
+
+  return `https://img.alexander.sbs/v1/image?t=${token}`
+
+}
