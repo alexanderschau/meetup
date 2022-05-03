533f6ab00dc3acc9346d506ecf8ccedad36074c0
Alexander Schau <alexander@schau.email>
2022-05-03 14:29:42 +0000

    format

--- src/pages/img/[data].ts
+++ src/pages/img/[data].ts
@@ -39,18 +39,4 @@ export async function get({ data }) {
       location: `https://img.alexander.sbs/v1/image?t=${token}`,
     },
   });
-  /*const resp = await fetch(
-    `https://img.alexander.sbs/v1/image?tkn=${
-      process.env.IMAGE_API_KEY
-    }&url=https://meetup.day/og&h=1000&w=1500&title=${encodeURIComponent(
-      meetup.title || "404 - Page not found"
-    )}&date=${encodeURIComponent(
-      new Date(meetup.from * 1000).toDateString() +
-        ", " +
-        moment(meetup.from * 1000)
-          .tz("UTC")
-          .format("HH.mm") +
-        " UTC" || ""
-    )}`
-  );*/
 }
