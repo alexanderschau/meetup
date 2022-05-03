bde192e36ed6118ee67c33bd69adc5d5db8d971e
Alexander Schau <alexander@schau.email>
2022-05-03 15:43:52 +0000

    update image url

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -8,7 +8,10 @@
 <meta name="title" content={data.title} />
 <meta name="description" content={data.description} />
 <meta name="keywords" content="" />
-<meta name="robots" content="index, follow" />
+<meta
+  name="robots"
+  content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="language" content="English" />
 <meta name="author" content="Alexander Schau" />
@@ -18,7 +21,10 @@
 <meta property="og:url" content={data.url} />
 <meta property="og:title" content={data.title} />
 <meta property="og:description" content={data.description} />
-<meta property="og:image" content={data.imgUrl} />
+<meta
+  property="og:image"
+  content={new URL(data.imgUrl, "https://meetup.day").toString()}
+/>
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
@@ -26,5 +32,5 @@
 <meta property="twitter:title" content={data.title} />
 <meta property="twitter:description" content={data.description} />
 <meta property="twitter:image" content={data.imgUrl} />
-<meta property="twitter:site" content="@alexanderschau" />
+<meta property="twitter:site" content="@meetup_day" />
 <meta property="twitter:creator" content="@alexanderschau" />
